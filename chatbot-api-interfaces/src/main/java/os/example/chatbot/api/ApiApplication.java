c709fc0fc88fff4822494197539e22e5727076ff
Afar8612 <justus8612@163.com>
2023-11-23 20:38:50 +0800

    范振益, feat: init project

---
+++ chatbot-api-interfaces/src/main/java/os/example/chatbot/api/ApiApplication.java
@@ -0,0 +1,15 @@
+package os.example.chatbot.api;
+
+//启动入口
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
